96d48439ac8322bc3e7f494b5ffa29f7e0dc72c6
timurt <borodatimur@gmail.com>
2022-04-17 23:33:28 +0300

    css js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,6 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .css('resources/css/owl-fix.css', 'public/css')
-    .css('resources/css/mansory.css', 'public/css')
-    .copy('resources/js/autocomplete.js', 'public/js')
-    .copy('resources/js/search.js', 'public/js')
     .copy('node_modules/bootstrap/dist', 'public/dist/bootstrap')
     .copy('node_modules/owl.carousel/dist', 'public/dist/owl-carousel')
     .copy('node_modules/jquery/dist', 'public/dist/jquery')
