e23301822d501b84040fc6071e1735d178f0629e
timurt <borodatimur@gmail.com>
2022-04-17 23:58:08 +0300

    search js

--- public/js/search.js
+++ public/js/search.js
@@ -1,32 +1,35 @@
-$('input[type=search]').autocomplete({
-    'source': function (request, response) {
-        if (request === '') return;
-        var base = $('base').attr('href');
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $.ajax({
-            url: base + 'search/',
-            dataType: 'json',
-            method: 'POST',
-            data: {
-                q: encodeURIComponent(request)
-            },
-            cache: false,
-            success: function (json) {
-                response($.map(json, function (item) {
-                    return {
-                        label: item['name'],
-                        value: item['value'],
-                        data: item['data']
-                    }
-                }));
-            }
-        });
-    },
-    'select': function (item) {
-        location.href = base + item.value;
-    }
+$(function() {
+    var base = $('base').attr('href');
+
+    $('input[type=search]').autocomplete({
+        'source': function (request, response) {
+            if (request === '') return;
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                url: base + 'search/',
+                dataType: 'json',
+                method: 'POST',
+                data: {
+                    q: request
+                },
+                cache: false,
+                success: function (json) {
+                    response($.map(json, function (item) {
+                        return {
+                            label: item['name'],
+                            value: item['value'],
+                            data: item['data']
+                        }
+                    }));
+                }
+            });
+        },
+        'select': function (item) {
+            location.href = base + item.value;
+        }
+    });
 });
